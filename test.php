1a6a22d25039eeb7b0ff168737506f8e164ef6ef
sindhu132 <127831367+sindhu132@users.noreply.github.com>
2023-03-14 23:49:43 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,intial-scale=1.0">
+    <meta http-equi="X=UA-Comatible" content="ie=edge".
+          </head>
+    <body>
+    <?php
+      echo "name: ";
+      echo $_POST['name']."<br";
+      echo "Email Id: ";
+      echo $_POST['email']."<br>";
+      echo "Password: ";
+      echo $_POST['pwd']."<br>";
+      echo "Date of Birth: ";
+      echo $_POST['birthday']."<br>";
+      
+  ?>
+    </body>
+    </html>
